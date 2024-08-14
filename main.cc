fea4742b1ec942b411788fad89cb243f90475fd6
Kelwan <austin@kelway.ca>
2024-08-14 10:21:53 -0700

    fix: Accomodate line endings and return exit codes on failure

--- main.cc
+++ main.cc
@@ -60,6 +60,22 @@ auto log_read_error() -> int {
   return 1;
 }
 
+enum LineEnding {
+  LF = 0,
+  CRLF = 1,
+};
+
+auto determine_line_ending(std::fstream &stream) -> LineEnding {
+  std::string first_line;
+  std::getline(stream, first_line);
+  if (first_line.ends_with("\r\n")) {
+    stream.seekg(0, std::ios_base::beg);
+    return LineEnding::CRLF;
+  }
+  stream.seekg(0, std::ios_base::beg);
+  return LineEnding::LF;
+}
+
 auto print_local_override(std::string dep_name) -> int {
 
   if (auto env_p = std::getenv("BUILD_WORKING_DIRECTORY")) {
@@ -76,13 +92,15 @@ auto print_local_override(std::string dep_name) -> int {
   auto absolute_module_path = absolute(*module_path);
   auto stream = std::fstream( //
       absolute_module_path,   //
-      std::ios_base::in | std::ios_base::out);
+      std::ios_base::in | std::ios_base::binary | std::ios_base::out);
 
   if (!stream) {
     std::cerr << "Stream attempted on an invalid file" << std::endl;
   }
 
   stream.seekg(0, std::ios_base::beg);
+
+  auto line_ending = determine_line_ending(stream);
 
   std::optional<std::string> dep_full_name = std::nullopt;
 
@@ -151,10 +169,17 @@ auto print_local_override(std::string dep_name) -> int {
       std::cerr << "ERR: " << rel_path << std::endl;
     }
 
-    stream << "\nlocal_path_override(\n"
-           << "\tmodule_name = \"" << *dep_full_name << "\",\n"
-           << "\tpath = \"" << rel_path.generic_string() << "\",\n"
-           << ")\n";
+    if (line_ending == LineEnding::CRLF) {
+      stream << "\r\nlocal_path_override(\r\n"
+             << "    " << "module_name = \"" << *dep_full_name << "\",\r\n"
+             << "    " << "path = \"" << rel_path.generic_string() << "\",\r\n"
+             << ")\r\n";
+    } else {
+      stream << "\nlocal_path_override(\n"
+             << "    " << "module_name = \"" << *dep_full_name << "\",\n"
+             << "    " << "path = \"" << rel_path.generic_string() << "\",\n"
+             << ")\n";
+    }
   } else {
     std::cerr << "Stream attempted on an invalid file or previous read "
                  "operation failed"
@@ -170,6 +195,9 @@ auto print_local_override(std::string dep_name) -> int {
 
 int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; ++i) {
-    return print_local_override(std::string(argv[i]));
+    auto exit_code = print_local_override(std::string(argv[i]));
+    if (exit_code != 0) {
+      return exit_code;
+    }
   }
 }
